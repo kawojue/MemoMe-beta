edba1b4afca21ad7ffc069ea85d944188d257f57
Raheem Kawojue <kawojue08@gmail.com>
2023-04-14 10:42:11 +0100

    [add] mailer

---
+++ backend/config/mailer.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+import nodemailer, { Transporter } from 'nodemailer'
+
+dotenv.config()
+
+export interface IMailer {
+    senderName: string
+    to: string
+    subject: string
+    text: string
+}
+
+const transporter: Transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    secure: true,
+    service: 'gmail',
+    requireTLS: true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PSWD
+    }
+})
+
+export default async function mailer({ senderName, to, subject, text }: IMailer): Promise<void> {
+    await transporter.sendMail({
+        from: `${senderName} <${process.env.EMAIL}>`,
+        to,
+        subject,
+        text,
+        headers: {
+            'Content-Type': 'application/text',
+        }
+    })
+}
