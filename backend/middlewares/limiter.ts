3d9595bc328084a5a4a54eac1728327a5016fb18
Raheem Kawojue <kawojue08@gmail.com>
2023-04-15 00:49:01 +0100

    [edit] limiter

--- backend/middlewares/limiter.ts
+++ backend/middlewares/limiter.ts
@@ -1,16 +1,20 @@
 import { Request, Response, NextFunction } from 'express'
 import rateLimit, { RateLimitRequestHandler, Options } from 'express-rate-limit'
 
-const timerArr: number[] = [23, 32, 19, 52, 42]
+const timerArr: number[] = [23, 32, 19, 52, 42] // random sec
 
 const limiter: RateLimitRequestHandler = rateLimit({
-    windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // random sec to try again
     max: 5, // max attempt
+    windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // try again in
     message: {
         message: 'Too many attempts. Please, try again later.'
     },
     handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
-        res.status(options.statusCode).send(options.message)
+        res.status(options.statusCode).json({
+            success: false,
+            action: "warning",
+            msg: options.message
+        })
     },
     standardHeaders: true,
     legacyHeaders: false,
