e2b73a161cf5aedebb31f771f01cb74f7d6b49ef
Raheem Kawojue <kawojue08@gmail.com>
2023-04-16 11:28:49 +0100

    .

--- backend/type.d.ts
+++ backend/type.d.ts
@@ -1,5 +1,14 @@
-export interface ICheckMail {
+interface ICheckMail {
     valid: boolean
     validators: any
     reason?: string
 }
+
+interface IMailer {
+    senderName: string
+    to: string
+    subject: string
+    text: string
+}
+
+export { IMailer, ICheckMail }
