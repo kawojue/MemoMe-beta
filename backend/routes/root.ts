657beea13d057ee36849fc7ae7b68f1cba3b6749
Raheem Kawojue <kawojue08@gmail.com>
2023-04-16 17:23:35 +0100

    [add] profile route

--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -1,11 +1,15 @@
 import apiRoute from './api/memome'
 import accountRoute from './account'
+import jwtVerify from '../middlewares/jwtVerify'
+import { getMemos } from '../controllers/memome'
 import express, { Router, Request, Response } from 'express'
 
 const rootRoute: Router = express.Router()
 
 rootRoute.use('/api', apiRoute)
 rootRoute.use('/account', accountRoute)
+
+rootRoute.get('/profile', jwtVerify, getMemos)
 
 rootRoute.get('/', (req: Request, res: Response) => {
     res.status(200).send("MemoMe")
