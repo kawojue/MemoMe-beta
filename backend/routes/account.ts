68ae20c4215c47ab98030ec0cab01af9db7b5007
Raheem Kawojue <kawojue08@gmail.com>
2023-04-17 13:09:07 +0100

    refractored

--- backend/routes/account.ts
+++ backend/routes/account.ts
@@ -1,14 +1,21 @@
 import express, { Router } from 'express'
-import limiter, { otpLimiter } from '../middlewares/limiter'
+import limiter from '../middlewares/limiter'
 import { 
     createUser, login,
     otpHandler 
 } from '../controllers/auth'
+import { ILimiter } from '../type'
 
 const accountRoute: Router = express.Router()
 
+const loginLimiter: ILimiter = {
+    max: 5,
+    timerArr: [23, 32, 19, 52, 42],
+    msg: "Too many attempts. Please, try again later."
+}
+
 accountRoute.post('/signup', createUser)
-accountRoute.post('/login', limiter, login)
-accountRoute.post('/req-otp', otpLimiter, otpHandler)
+accountRoute.post('/login', limiter(loginLimiter), login)
+accountRoute.post('/req-otp', limiter({max: 1, timerArr: [30, 60, 90]}), otpHandler)
 
 export default accountRoute
