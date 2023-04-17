8bf3ea1ef965d2c468d6ecc2abf739643896cdb6
Raheem Kawojue <kawojue08@gmail.com>
2023-04-17 14:17:21 +0100

    .

---
+++ frontend/src/app/hehe/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Hehe: React.FC = () => {
+    return (
+        <h1>
+            Hehe
+        </h1>
+    )
+}
+
+export default Hehe
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      
+      Memo Me
     </main>
   )
 }
