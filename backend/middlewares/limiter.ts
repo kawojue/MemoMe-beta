85b171d75668c6d3d35ed2363d2c615fe3faa1f0
Raheem Kawojue <kawojue08@gmail.com>
2023-04-17 13:08:22 +0100

    refractored limiter

--- backend/middlewares/limiter.ts
+++ backend/middlewares/limiter.ts
@@ -1,59 +1,24 @@
+import { ILimiter } from '../type'
 import { Request, Response, NextFunction } from 'express'
 import rateLimit, { RateLimitRequestHandler, Options } from 'express-rate-limit'
 
-const timerArr: number[] = [23, 32, 19, 52, 42] // random sec
+export default function limiterFunc({ max, timerArr, msg = "Too many requests sent." }: ILimiter): RateLimitRequestHandler {
+    const limiter: RateLimitRequestHandler = rateLimit({
+        max, // max attempt
+        windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // try again in
+        message: {
+            message: msg
+        },
+        handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
+            res.status(options.statusCode).json({
+                success: false,
+                action: "warning",
+                msg: options.message
+            })
+        },
+        standardHeaders: true,
+        legacyHeaders: false,
+    })
 
-const limiter: RateLimitRequestHandler = rateLimit({
-    max: 5, // max attempt
-    windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // try again in
-    message: {
-        message: 'Too many attempts. Please, try again later.'
-    },
-    handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
-        res.status(options.statusCode).json({
-            success: false,
-            action: "warning",
-            msg: options.message
-        })
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
-})
-
-const msgTimerArr = [4, 5, 7]
-export const msgLimiter: RateLimitRequestHandler = rateLimit({
-    max: 1, // max attempt
-    windowMs: msgTimerArr[Math.floor(Math.random() * msgTimerArr.length)] * 1000, // try again in
-    message: {
-        message: 'Too much requests sent.'
-    },
-    handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
-        res.status(options.statusCode).json({
-            success: false,
-            action: "warning",
-            msg: options.message
-        })
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
-})
-
-const otpTimerArr = [30, 60, 90]
-export const otpLimiter: RateLimitRequestHandler = rateLimit({
-    max: 1, // max attempt
-    windowMs: otpTimerArr[Math.floor(Math.random() * otpTimerArr.length)] * 1000, // try again in
-    message: {
-        message: 'Too much requests sent.'
-    },
-    handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
-        res.status(options.statusCode).json({
-            success: false,
-            action: "warning",
-            msg: options.message
-        })
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
-})
-
-export default limiter
+    return limiter
+}
