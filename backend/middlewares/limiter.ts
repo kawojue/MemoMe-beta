ac62e9786714eefcc0fc820cbb587bf054ec25db
Raheem Kawojue <kawojue08@gmail.com>
2023-04-17 03:51:10 +0100

    [add] req-otp limiter

--- backend/middlewares/limiter.ts
+++ backend/middlewares/limiter.ts
@@ -38,4 +38,22 @@ export const msgLimiter: RateLimitRequestHandler = rateLimit({
     legacyHeaders: false,
 })
 
+const otpTimerArr = [30, 60, 90]
+export const otpLimiter: RateLimitRequestHandler = rateLimit({
+    max: 1, // max attempt
+    windowMs: otpTimerArr[Math.floor(Math.random() * otpTimerArr.length)] * 1000, // try again in
+    message: {
+        message: 'Too much requests sent.'
+    },
+    handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
+        res.status(options.statusCode).json({
+            success: false,
+            action: "warning",
+            msg: options.message
+        })
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+})
+
 export default limiter
