a2d40ff2ba90bf380b3371ad9dc1c057cdada492
Raheem Kawojue <kawojue08@gmail.com>
2023-04-17 03:51:37 +0100

    [add] req-otp route

--- backend/routes/account.ts
+++ backend/routes/account.ts
@@ -1,10 +1,14 @@
 import express, { Router } from 'express'
-import limiter from '../middlewares/limiter'
-import { createUser, login } from '../controllers/auth'
+import limiter, { otpLimiter } from '../middlewares/limiter'
+import { 
+    createUser, login,
+    otpHandler 
+} from '../controllers/auth'
 
 const accountRoute: Router = express.Router()
 
 accountRoute.post('/signup', createUser)
 accountRoute.post('/login', limiter, login)
+accountRoute.post('/req-otp', otpLimiter, otpHandler)
 
 export default accountRoute
