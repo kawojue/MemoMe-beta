9e492a32e7349d851ae59ccebc539c1acb26e2be
Raheem Kawojue <kawojue08@gmail.com>
2023-04-17 03:17:56 +0100

    ..ongoing

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,9 @@ import mongoose from 'mongoose'
 import DBConn from './config/DBConn'
 import rootRoute from './routes/root'
 import corsOptions from './config/corsOptions'
+const { auth } = require('express-openid-connect')
 import express, { Application } from 'express'
+import { auth0Config } from './config/auth0Config'
 
 const app: Application = express()
 const PORT = process.env.PORT || 1707
@@ -18,7 +20,10 @@ DBConn(process.env.DB_URI as string)
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors(corsOptions))
+// app.use(auth(auth0Config))
 app.use(express.urlencoded({ extended: false }))
+
+
 
 // set route
 app.use('/', rootRoute)
