bfb933df13e0c1b65b9afd8c5425321cd9ba50e0
Raheem Kawojue <kawojue08@gmail.com>
2023-04-19 00:08:51 +0100

    [add] screens and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "clr-0": "rgb(51, 51, 51)",
+        "clr-1": "rgb(100, 92, 255)"
+      },
+      screens: {
+        sm: "600px",
+        md: "800px",
+        lg: "1200px"
+      }
+    },
   },
   plugins: [],
 }
