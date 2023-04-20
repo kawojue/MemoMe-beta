025f5b185d86d0e0da87222e79076a31edb5268d
Raheem Kawojue <kawojue08@gmail.com>
2023-04-20 09:06:04 +0100

    [add] cloudinry config

---
+++ backend/config/cloudinary.ts
@@ -0,0 +1,7 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+export default cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+})
