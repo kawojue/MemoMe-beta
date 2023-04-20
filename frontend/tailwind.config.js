d5ec335aad7710e8aa2946f6cd61d4147d314575
Raheem Kawojue <kawojue08@gmail.com>
2023-04-20 13:51:45 +0100

    [add] tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         "clr-0": "rgb(51, 51, 51)",
         "clr-1": "rgb(100, 92, 255)"
       },
+      fontFamily: {
+        kaushan: ["Kaushan Script", 'cursive'],
+        poppins: ["Poppins", 'san-serif']
+      },
       screens: {
         sm: "600px",
         md: "800px",
