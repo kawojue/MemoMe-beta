30b2f07327ab029f53db81e9e4c0e5127effdc82
Raheem Kawojue <kawojue08@gmail.com>
2023-04-20 15:14:46 +0100

    .

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout'
 import { AuthProvider } from '@/hooks/useContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AuthProvider>
   )
 }
