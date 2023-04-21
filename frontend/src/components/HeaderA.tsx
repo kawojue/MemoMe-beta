3b10554f1c1922b567c169032e5e7541afc8426b
Raheem Kawojue <kawojue08@gmail.com>
2023-04-21 14:21:46 +0100

    [edit]

--- frontend/src/components/HeaderA.tsx
+++ frontend/src/components/HeaderA.tsx
@@ -8,13 +8,7 @@ const HeaderA = () => {
                     <span className="text-clr-1">Memo</span>
                     <span className="text-black">Me</span>
                 </div>
-                <ul className="md:flex gap-10 hidden">
-                    <li>
-                        <Link href="/signup"
-                        className="text-xl text-clr-3 font-poppins">
-                            Sign Up
-                        </Link>
-                    </li>
+                <ul>
                     <li>
                         <Link href="/login"
                         className="text-xl text-white bg-clr-2 rounded-lg px-3 py-1 font-poppins hover:bg-clr-1 hover:text-clr-3 trans">
@@ -22,11 +16,6 @@ const HeaderA = () => {
                         </Link>
                     </li>
                 </ul>
-                <button type="button" className="block hamburger md:hidden focus:outline-none trans">
-                    <span className="hamburger-top trans"></span>
-                    <span className="hamburger-middle trans"></span>
-                    <span className="hamburger-bottom trans"></span>
-                </button>
             </nav>
         </header>
     )
