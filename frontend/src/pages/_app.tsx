1a360e32cb17e605c3c7563f132675fc941b15cf
Raheem Kawojue <kawojue08@gmail.com>
2023-04-21 14:24:17 +0100

    .

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
-import { AuthProvider } from '@/hooks/useContext'
+import { AuthProvider } from '@/hooks/useAuth'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
