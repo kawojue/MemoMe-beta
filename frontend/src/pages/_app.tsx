5000237f4975654f7493bd4ec6198d7b22726e69
Raheem Kawojue <kawojue08@gmail.com>
2023-04-22 00:02:22 +0100

    [add] toastify css

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { AuthProvider } from '@/hooks/useAuth'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
