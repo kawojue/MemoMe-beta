17c7f70d3533c028872ea028a05a08b00a1314b6
Raheem Kawojue <kawojue08@gmail.com>
2023-04-22 17:54:40 +0100

    increase file limit

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import cors from 'cors'
-import morgan from 'morgan'
+import logger from 'morgan'
 import mongoose from 'mongoose'
 import DBConn from './config/DBConn'
 import rootRoute from './routes/root'
-import fileUpload from 'express-fileupload'
 import corsOptions from './config/corsOptions'
 import express, { Application } from 'express'
 
@@ -16,13 +15,10 @@ const PORT = process.env.PORT || 1707
 DBConn(process.env.DB_URI as string)
 
 // set middlewares
-app.use(fileUpload())
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(logger('dev'))
+app.use(express.json({ limit: '10mb'}))
 app.use(cors(corsOptions))
-app.use(express.urlencoded({ extended: false }))
-
-
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
 // set route
 app.use('/', rootRoute)
