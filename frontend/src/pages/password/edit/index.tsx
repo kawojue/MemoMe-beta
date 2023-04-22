3ef1daa065d953d9dd862c28afcf3b0b11c968a6
Raheem Kawojue <kawojue08@gmail.com>
2023-04-23 00:11:38 +0100

    [add] password reset

--- frontend/src/pages/password/edit/index.tsx
+++ frontend/src/pages/password/edit/index.tsx
@@ -1,6 +1,60 @@
+import Meta from "@/components/Meta"
+import useAuth from "@/hooks/useAuth"
+import Header from "@/components/HeaderA"
+import PswdButton from '@/components/PswdBtn'
+import { ToastContainer } from 'react-toastify'
+
 const Edit = () => {
+    const {
+        showPswd, pswd, setPswd,
+        setShowPswd, confirmPswd,
+        setConfirmPswd, validPswd,
+        handlePswdReset, btnLoading
+    }: any = useAuth()
+
     return (
-        <></>
+        <>
+            <Meta title="Reset Password" />
+            <Header />
+            <ToastContainer />
+            <form onSubmit={(e) => e.preventDefault()}
+            className="mx-auto w-[90vw] max-w-[500px] rounded-md mt-12 signup py-5 px-6">
+                <h1 className="text-clr-5 text-center font-semibold tracking-wider text-2xl md:text-4xl">
+                    Reset Password
+                </h1>
+                <article className="mt-5 form-center">
+                    <div className="form-group">
+                        <label>Password</label>
+                        <div className="pswd-container">
+                            <input max={32} value={pswd}
+                            onChange={(e) => setPswd(e.target.value)}
+                            type={`${showPswd ? 'text': 'password'}`}
+                            className={`border-2 ${validPswd ?
+                            'border-green-400' : 'border-red-400'}`}
+                            aria-invalid={validPswd ? "false" : "true"}
+                            aria-describedby="uidnote" />
+                            <PswdButton get={showPswd} set={setShowPswd} />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm Password</label>
+                        <div className="pswd-container">
+                            <input max={32} value={confirmPswd}
+                            type='password' aria-describedby="uidnote"
+                            onChange={(e) => setConfirmPswd(e.target.value)}
+                            className={`border-2 ${validPswd ?
+                            'border-green-400' : 'border-red-400'}`}
+                            aria-invalid={validPswd ? "false" : "true"} />
+                        </div>
+                    </div>
+                    <button
+                    className="btn" disabled={!validPswd}
+                    onClick={async () => await handlePswdReset()}>
+                        {btnLoading ? 'Encrypting..' : 'Reset Password'}
+                    </button>
+                </article>
+            </form>
+        </>
     )
 }
 
