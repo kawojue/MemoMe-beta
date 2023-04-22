fb4aba29377214a7eb9702d9a27b3db022863007
Raheem Kawojue <kawojue08@gmail.com>
2023-04-22 14:11:06 +0100

    [fix] typo

--- frontend/src/components/Meta.tsx
+++ frontend/src/components/Meta.tsx
@@ -19,7 +19,7 @@ const Meta: React.FC<IMeta> = ({
             <link rel="icon" href="/favicon.ico" />
             <meta name="keywords" content={keywords} />
             <meta name="description" content={description} />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
     )
