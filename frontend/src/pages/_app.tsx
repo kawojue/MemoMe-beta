fe5ffbdfadc40c9604ace1cabf029d8ecd4515f0
Raheem Kawojue <kawojue08@gmail.com>
2023-04-22 15:41:40 +0100

    [add] toast container

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { AuthProvider } from '@/hooks/useAuth'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps}>
+          <ToastContainer />
+        </Component>
       </Layout>
     </AuthProvider>
   )
