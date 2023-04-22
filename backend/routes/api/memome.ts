1e2579b2b78258b4e71e1831363dee8c425509e4
Raheem Kawojue <kawojue08@gmail.com>
2023-04-23 00:03:32 +0100

    [refractored

--- backend/routes/api/memome.ts
+++ backend/routes/api/memome.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express'
 import limiter from '../../middlewares/limiter'
-import { addMemo, countViews } from '../../controllers/memome'
+import { addMemo, getUser } from '../../controllers/memome'
 
 const apiRoute: Router = express.Router()
 
 apiRoute.route('/:user')
-    .get(countViews)
+    .get(getUser)
     .post(limiter({max: 1, timerArr: [4, 5, 7]}), addMemo)
 
 export default apiRoute
