d099b4f84cd1c69f48c702aa1b0801ece8dd90d6
Raheem Kawojue <kawojue08@gmail.com>
2023-04-22 16:44:00 +0100

    .

--- backend/config/auth0.ts
+++
@@ -1,8 +0,0 @@
-export const auth0Config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.JWT_SECRET as string,
-    baseURL: 'http://localhost:1707',
-    clientID: process.env.AUTH_CLIENT_ID as string,
-    issuerBaseURL: 'https://dev-r80r0y5b5nhd8ca2.us.auth0.com'
-}
--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -1,7 +1,8 @@
-import HeaderA from "./HeaderA"
-import { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import Header from "./HeaderA"
 import useAuth from "@/hooks/useAuth"
 import axios from '@/pages/api/instance'
+import { useState, useEffect } from 'react'
 import { ToastContainer } from "react-toastify"
 
 const User: React.FC<{ user: string }> = ({ user }) => {
@@ -9,39 +10,77 @@ const User: React.FC<{ user: string }> = ({ user }) => {
 
     const [media, setMedia] = useState<string>("")
     const [content, setContent] = useState<string>("")
+    const [textCounter, setTextCounter]= useState<number>(650)
 
-    const convertFile = (file: any): void => {
-        const reader: FileReader = new FileReader()
-        reader.readAsDataURL(file)
-        reader.onload = () => {
-            setMedia(reader.result as string)
+    const handleContent = (e: any): void => {
+        const value: string = e.target.value
+        if (value.length <= 650) {
+            setContent(value)
         }
     }
 
     const handleMedia = (e: any): void => {
-        const file: any = e.target.files[0]
-        convertFile(file)
+        const imageFile = e.target.files[0]
+        setMedia(imageFile)
+        const formData: FormData = new FormData()
+        formData.append('image', media)
     }
+
+    // This works but for smaller files
+    // const checkFile = (file: any) => {
+    //     if (!file) {
+    //         return false
+    //     }
+    //     const maxSize: number = 5242880 // 5MB
+    //     const { name, size }: any = file
+    //     const allowedFormats: string[] = ['jpg', 'jpeg', 'png', 'heif']
+    //     const getFormat = name.split('.')[1]
+    //     if (allowedFormats.includes(getFormat) && size <= maxSize) {
+    //         return true
+    //     }
+    //     return false
+    // }
+
+    // const convertFile = (file: any): void => {
+    //     const reader: FileReader = new FileReader()
+    //     reader.readAsDataURL(file)
+    //     reader.onload = () => {
+    //         setMedia(reader.result as string)
+    //     }
+    // }
+
+    // const handleMedia = (e: any): void => {
+    //     const file: any = e.target.files[0]
+    //     if (checkFile(file)) {
+    //         convertFile(file)
+    //     } else {
+    //         notify('warning', "File format or size is not allowed.")
+    //     }
+    // }
+
+    useEffect(() => {
+        setTextCounter(textCounter - 1)
+    }, [content])
 
     const handleMessage = async () => {
         await axios.post(`/api/${user}`, JSON.stringify({ media, content }))
         .then((res: any) => {
-            notify(res.data.action, res.data.msg)
+            notify(res?.data.action, res?.data.msg)
         })
         .catch((err: any) => {
-            notify(err.response.data.action, err.response.data.msg)
+            notify(err.response?.data?.action, err.response?.data?.msg)
         })
     }
 
     return (
         <>
-            <HeaderA/>
+            <Header />
+            <ToastContainer />
             <form className="" onSubmit={(e) => e.preventDefault()}>
-                <ToastContainer />
                 <h1>Send anonymous message to <span>{user}</span></h1>
                 <article>
-                    <textarea className="resize-none"
-                    value={content} onChange={(e) => setContent(e.target.value)} />
+                    <textarea className="resize-none" maxLength={650}
+                    value={content} onChange={(e) => handleContent(e)} />
                     <input type="file" onChange={(e) => handleMedia(e)} />
                 </article>
                 <button onClick={async () => await handleMessage()}>
