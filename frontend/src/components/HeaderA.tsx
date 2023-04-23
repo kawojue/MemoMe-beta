7938e2cdc98cf0d4d291e0c854660947a345a80c
Raheem Kawojue <kawojue08@gmail.com>
2023-04-23 15:33:24 +0100

    refractored

--- frontend/src/components/HeaderA.tsx
+++ frontend/src/components/HeaderA.tsx
@@ -1,17 +1,16 @@
+
+import Home from './Home'
 import Link from 'next/link'
 
 const HeaderA = ({ get = "login" }: { get?: string }) => {
     return (
         <header className="w-full">
-            <nav className="flex justify-between items-center mx-auto md:px-10 px-7 py-5">
-                <Link href="/" className="text-4xl md:text-5xl font-kaushan">
-                    <span className="text-clr-1">Memo</span>
-                    <span className="text-black">Me</span>
-                </Link>
+            <nav className="nav md:px-10">
+                <Home />
                 <ul>
                     <li>
                         <Link href={`/${get.replace(" ", "")}`}
-                        className="text-xl text-white bg-clr-2 rounded-lg px-3 py-1 font-poppins hover:bg-clr-1 hover:text-clr-3 trans">
+                        className="nav-btn bg-clr-2 hover:bg-clr-1 hover:text-clr-3 trans">
                             {`${get.charAt(0).toUpperCase()}${get.slice(1)}`}
                         </Link>
                     </li>
