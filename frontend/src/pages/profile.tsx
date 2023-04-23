0f5f3bed96274aba404cf3d61c3eb87920ba52ca
Raheem Kawojue <kawojue08@gmail.com>
2023-04-23 15:32:50 +0100

    more on it

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,6 +1,24 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import Meta from '@/components/Meta'
+import Profile from '@/components/Profile'
+import { useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import { useRouter, NextRouter } from 'next/router'
+
 const profile = () => {
+  const router: NextRouter = useRouter()
+  const [token, setToken] = useState<string>("")
+
+  useEffect(() => {
+    setToken(JSON.parse(localStorage.getItem("token") as any) || {})
+  }, [router])
+
   return (
-    <div>profile</div>
+    <>
+      <ToastContainer />
+      <Meta title="Profile" />
+      <Profile token={token}/>
+    </>
   )
 }
 
