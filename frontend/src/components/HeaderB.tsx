1b2f4248f4c6e05126317d1d99e0fc344034c5c2
Raheem Kawojue <kawojue08@gmail.com>
2023-04-23 15:33:16 +0100

    refractored

--- frontend/src/components/HeaderB.tsx
+++ frontend/src/components/HeaderB.tsx
@@ -1,29 +1,18 @@
-import useAuth from '@/hooks/useAuth'
+import Home from './Home'
 
-const HeaderB = () => {
-    const { aside, setAside }: any = useAuth()
-
+const HeaderB:React.FC<{ logout: () => Promise<void>}> = ({ logout }) => {
     return (
         <header className="w-full">
-            <nav className="flex justify-between items-center mx-auto md:px-10 px-7 py-5">
-                <div className="text-4xl md:text-5xl font-kaushan">
-                    <span className="text-clr-1">Memo</span>
-                    <span className="text-black">Me</span>
-                </div>
-                <ul className="md:flex gap-10 hidden">
+            <nav className="nav md:px-10">
+                <Home/>
+                <ul>
                     <li>
-                        <button type="button"
-                        className="text-xl text-white bg-red-500 rounded-lg px-3 py-1 font-poppins hover:bg-red-600 hover:text-clr-3 trans tracking-wider">
+                        <button type="button" onClick={async () => await logout()}
+                        className="nav-btn  bg-red-500 hover:bg-red-600 hover:text-clr-3">
                             Logout
                         </button>
                     </li>
                 </ul>
-                <button type="button" onClick={() => setAside(!aside)}
-                className={`${aside ? 'open' : ''} block hamburger md:hidden focus:outline-none trans`}>
-                    <span className="hamburger-top trans"></span>
-                    <span className="hamburger-middle trans"></span>
-                    <span className="hamburger-bottom trans"></span>
-                </button>
             </nav>
         </header>
     )
