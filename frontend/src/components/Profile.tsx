eed30baecdcf22248132ce0a3f1162bb2397aaaf
Raheem Kawojue <kawojue08@gmail.com>
2023-04-24 16:08:56 +0100

    .

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,47 +1,13 @@
-import Header from "./HeaderB"
 import { useEffect } from 'react'
 import useAuth from "@/hooks/useAuth"
+import decrypt from '@/utils/decryption'
 import axios from "@/pages/api/instance"
-import { NextRouter, useRouter } from "next/router"
 
-const Profile: React.FC<{ token: string }> = ({ token }) => {
-    const { notify }: any = useAuth()
-    const router: NextRouter = useRouter()
-
-    const handleLogout = async (): Promise<void> => {
-        console.log(token)
-        await axios.get('/account/logout', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        }).then((res: any) => {
-            localStorage.clear()
-            router.push("/")
-        }).catch((err: any) => {
-            notify("error", err.code)
-        })
-    }
-
-    useEffect(() => {
-        const handleProfile = async (): Promise<void> => {
-            await axios.get('/profile', {
-                headers: {
-                    'Authorization': `Bearer ${token}` 
-                }
-            }).then((res: any) => {
-                console.log(res)
-            }).catch((err: any) => {
-                // router.push('/login')
-                console.error(err)
-            })
-        }
-        (async () => await handleProfile())()
-    }, [router, token])
+const Profile: React.FC<{ token?: string }> = ({ token }) => {
 
     return (
-        <main>
-            <Header logout={handleLogout} />
-        </main>
+        <section>
+        </section>
     )
 }
 
