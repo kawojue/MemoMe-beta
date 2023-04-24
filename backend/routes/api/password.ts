aade5cdaf7e823db72dbee5421f11eb5734246fd
Raheem Kawojue <kawojue08@gmail.com>
2023-04-24 16:37:59 +0100

    [add] editpswd route

--- backend/routes/api/password.ts
+++ backend/routes/api/password.ts
@@ -1,9 +1,12 @@
 import express, { Router } from 'express'
+import jwtVerify from '../../middlewares/jwtVerify'
+import { editPswd } from '../../controllers/settings'
 import { resetpswd, verifyOTP } from '../../controllers/auth'
 
 const passwordRoute: Router = express.Router()
 
 passwordRoute.post('/reset', resetpswd)
 passwordRoute.post('/verify', verifyOTP)
+passwordRoute.post('/edit', jwtVerify, editPswd)
 
 export default passwordRoute
