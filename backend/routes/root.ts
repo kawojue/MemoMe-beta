f1cf35d922fa38569711c220fdf2be92f71f8d93
Raheem Kawojue <kawojue08@gmail.com>
2023-04-24 16:54:36 +0100

    [use] settings route

--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -1,5 +1,6 @@
 import apiRoute from './api/memome'
 import accountRoute from './account'
+import settingsRoute from './api/settings'
 import jwtVerify from '../middlewares/jwtVerify'
 import { getMemos } from '../controllers/memome'
 import express, { Router, Request, Response } from 'express'
@@ -8,6 +9,7 @@ const rootRoute: Router = express.Router()
 
 rootRoute.use('/api', apiRoute)
 rootRoute.use('/account', accountRoute)
+rootRoute.use('/settings', settingsRoute)
 
 rootRoute.get('/profile', jwtVerify, getMemos)
 
