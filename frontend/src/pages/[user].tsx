60ec571f8f0bff01d2c4f52a709b37f36369ca80
Raheem Kawojue <kawojue08@gmail.com>
2023-04-25 16:05:40 +0100

    [fix] double res

--- frontend/src/pages/[user].tsx
+++ frontend/src/pages/[user].tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react'
 import IsUser from '@/components/User'
 import axios from '@/pages/api/instance'
 import { useRouter, NextRouter } from 'next/router'
@@ -9,8 +11,18 @@ const User = ({ data }: { data: any }) => {
         router.push("/404")
     }
 
+    const countViews = async ():Promise<void> => {
+        await axios.post('/api/user/count-views', JSON.stringify({ user: data.user }))
+        .then((res: any) => {})
+        .catch((err: any) => {})
+    }
+
+    useEffect(() => {
+        (async () => await countViews())()
+    }, [])
+
     return (
-        <IsUser data={data} />
+        <IsUser data={data.tempData} />
     )
 }
 
@@ -18,7 +30,7 @@ export const getServerSideProps = async (query: any) => {
     let data: any = {}
     await axios.get(`/api/${query.params.user}`)
     .then((res: any) => {
-        data = res.data
+        data = { user: res?.data.user, tempData: res.data }
     })
     .catch((err: any) => {
         data = { err: err?.code || '' }
