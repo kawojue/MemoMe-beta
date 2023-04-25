c1b71e3f33ded2e2e3246a94a5db74111a08e31c
Raheem Kawojue <kawojue08@gmail.com>
2023-04-25 16:05:05 +0100

    [add] profile views count

--- backend/routes/api/memome.ts
+++ backend/routes/api/memome.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express'
 import limiter from '../../middlewares/limiter'
-import { addMemo, getUser } from '../../controllers/memome'
+import { addMemo, countViews, getUser } from '../../controllers/memome'
 
 const apiRoute: Router = express.Router()
 
 apiRoute.route('/:user')
     .get(getUser)
-    .post(limiter({max: 1, timerArr: [4, 5, 7]}), addMemo)
+    .post(limiter({max: 1, timerArr: [4, 5, 7]}), addMemo);
+
+apiRoute.post('/user/count-views', countViews)
 
 export default apiRoute
