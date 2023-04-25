80244151c69930733218d1a7d69d719713519c59
Raheem Kawojue <kawojue08@gmail.com>
2023-04-25 01:32:46 +0100

    .

--- frontend/src/pages/api/instance.ts
+++ frontend/src/pages/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL: string = process.env.NODE_ENV === 'production' ? process.env.BACKEND_URL as string : 'http://localhost:1707'
+const BASE_URL: string = process.env.NODE_ENV === 'production' ? 'https://memome-backend.vercel.app' as string : 'http://localhost:1707'
 
 export default axios.create({
   baseURL: BASE_URL,
