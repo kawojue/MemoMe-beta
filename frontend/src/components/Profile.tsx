7545f7b01696a8c66c082f4d5ffe32e36f14a92f
Raheem Kawojue <kawojue08@gmail.com>
2023-04-26 17:47:10 +0100

    more on it..

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,18 +2,18 @@
 import useAuth from "@/hooks/useAuth"
 import { FaShare } from 'react-icons/fa'
 import decrypt from '@/utils/decryption'
-import axios from "@/pages/api/instance"
+import { AiFillEye } from 'react-icons/ai'
 import { useState, useEffect } from 'react'
 import { useRouter, NextRouter } from 'next/router'
 
 const Profile: React.FC<{ data: any }> = ({ data }) => {
     const { notify }: any = useAuth()
     const router: NextRouter = useRouter()
-    const [copy, setCopy] = useState<any>(<FaShare />)
-    const [memos, setMemos] = useState<any[]>([])
     const [user, setUser] = useState<string>('')
+    const [memos, setMemos] = useState<any[]>([])
     const [views, setViews] = useState<number>(0)
-
+    const [copy, setCopy] = useState<any>(<FaShare />)
+    
     useEffect(() => {
         setMemos(data?.memos?.body)
         setUser(data?.account?.user)
@@ -34,17 +34,42 @@ const Profile: React.FC<{ data: any }> = ({ data }) => {
     }
 
     return (
-        <main className="mt-5 relative">
-            <p className="text-center font-semibold text-2xl md:text-3xl tracking-wider text-clr-3 font-poppins">Messages</p>
+        <main className="mt-5 mb-10 relative">
+            <div className="flex flex-col gap-2 mb-10 items-center justify-center">
+                <h1 className="text-center font-semibold text-2xl md:text-3xl tracking-wider text-clr-3 font-poppins">
+                    Messages
+                </h1>
+                <div className="w-32 h-1.5 bg-clr-2 rounded-md"></div>
+            </div>
             <button className="absolute top-0 text-clr-0 md:right-2 right-6 px-5 py-2 bg-clr-1 rounded-md trans hover:bg-clr-2 hover:text-clr-5"
             onClick={async () => await onCopy(`www.memome.one/${user}`)}>{copy}</button>
-            <section>
+            <p className="flex items-center gap-3 px-2 py-1 bg-clr-6 w-fit rounded-md">
+                <AiFillEye />
+                <span>{views}</span>
+            </p>
+            <section className="profile-msgs text-left">
                 {memos?.map((memo: any) => (
-                    <article key={memo.idx}>
-                        {memo?.media && <img
-                        src={memo?.media?.secure_url}
-                        alt={memo?.idx}/>}
-                        {memo?.content && <p>{decrypt(memo?.content || '')}</p>}
+                    <article key={memo.idx} className="profile-msg">
+                        <div className="media">
+                            {memo?.media && <img
+                            className="image"
+                            src={memo?.media?.secure_url}
+                            alt={memo?.idx}/>}
+                        </div>
+                        {memo?.content &&
+                        <div className="content">
+                            {decrypt(memo?.content || '').split('\n')?.map(
+                                (content: string, index: number) => (
+                                    <p className="texts" key={index}>{String(content)}</p>
+                            ))}
+                        </div>}
+                        {memo?.media &&
+                        <div className="mt-2 flex justify-center">
+                            <a href="https://res.cloudinary.com/kawojue/image/upload/v1682526539/MemoMe/6449383785ef04c71e27edf9/qrkyp2ofnjl5bwnxfwuf.jpg" download
+                            className="bg-clr-3 rounded-lg px-2 py-1 w-full font-semibold font-poppins">
+                                Download original Image
+                            </a>
+                        </div>}
                     </article>
                 ))}
             </section>
