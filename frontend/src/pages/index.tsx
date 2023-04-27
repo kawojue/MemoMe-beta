ef672bf20921639b052cf96bb4feeb60278d3db5
Raheem Kawojue <kawojue08@gmail.com>
2023-04-27 09:54:08 +0100

    refractored

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     <>
       <Header />
       <main className="mt-28 mx-auto grid place-items-center">
-        <p className='text-2xl md:text-4xl lg:text-6xl flex flex-col w-full items-center select-none gap-1.5 font-medium text-center'>
+        <h1 className='home-h1 md:text-4xl lg:text-6xl'>
           <span>Send, Recieve, and Share</span>
           <span>Anonymous</span>
           <span>Messages with your Friends.</span>
-        </p>
+        </h1>
         <Link href="/signup"
-        className="rounded-lg w-fit bg-clr-1 flex items-center gap-3 py-3 px-5 font-medium mt-10 tracking-wider text-xl hover:bg-clr-2 trans" >
+        className="signup-link hover:bg-clr-2">
           {`Let's go..`}
           <BsFillRocketTakeoffFill />
         </Link>
