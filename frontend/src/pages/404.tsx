091f754ee70d856b29ca90ea7958a6f9b855bc48
Raheem Kawojue <kawojue08@gmail.com>
2023-04-27 09:54:15 +0100

    refractored

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -7,11 +7,11 @@ const NotFound = () => {
         <>
             <Header />
             <main className="mt-28 mx-auto grid place-items-center">
-                <p className='text-2xl md:text-4xl lg:text-6xl flex flex-col w-full items-center select-none gap-1.5 font-medium text-center'>
-                <span>Keep rolling your eyes.</span>
-                <span>May be you will find the</span>
-                <span>Page somewhere.</span>
-                </p>
+                <h1 className='home-h1 md:text-4xl lg:text-6xl'>
+                    <span>Keep rolling your eyes.</span>
+                    <span>May be you will find the</span>
+                    <span>Page somewhere.</span>
+                </h1>
                 <Link href="/signup"
                 className="rounded-lg w-fit bg-clr-1 flex items-center gap-3 py-3 px-5 font-medium mt-10 tracking-wider text-xl hover:bg-clr-2 trans" >
                 {`Let's get you Signed up..`}
