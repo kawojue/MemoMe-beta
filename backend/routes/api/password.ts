86ce348880bcf001f3f4401df11d227c481bebdd
Raheem Kawojue <kawojue08@gmail.com>
2023-04-27 14:24:37 +0100

    [add] limiter

--- backend/routes/api/password.ts
+++ backend/routes/api/password.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express'
+import limiter from '../../middlewares/limiter'
 import jwtVerify from '../../middlewares/jwtVerify'
 import { editPswd } from '../../controllers/settings'
 import { resetpswd, verifyOTP } from '../../controllers/auth'
@@ -6,7 +7,7 @@ import { resetpswd, verifyOTP } from '../../controllers/auth'
 const passwordRoute: Router = express.Router()
 
 passwordRoute.post('/reset', resetpswd)
-passwordRoute.post('/verify', verifyOTP)
 passwordRoute.post('/edit', jwtVerify, editPswd)
+passwordRoute.post('/verify', limiter({max:3, timerArr:[14, 9, 15]}), verifyOTP)
 
 export default passwordRoute
