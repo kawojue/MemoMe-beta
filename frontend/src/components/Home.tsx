dba15865f20892350eb05952e121521d3e7c3314
Raheem Kawojue <kawojue08@gmail.com>
2023-04-27 09:54:38 +0100

    refractored

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
     return (
         <Link href="/" className="text-4xl md:text-5xl font-kaushan">
             <span className="text-clr-1">Memo</span>
-            <span className="text-black">Me</span>
+            <span className="text-clr-3">Me</span>
         </Link>
     )
 }
