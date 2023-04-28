7389d1d29f79166f391b88e141d9a66f0aa5411b
Raheem Kawojue <kawojue08@gmail.com>
2023-04-28 09:55:22 +0100

    [add] username handler

--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -1,6 +1,48 @@
-const Account: React.FC<{ data: any }> = ({ data }) => {
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from 'react'
+import useAuth from "@/hooks/useAuth"
+
+const Account: React.FC<{ token: string }> = ({ token }) => {
+  const { user, setUser, validUser, userRef,
+    btnLoading, handleUsername }: any = useAuth()
+
   return (
-    <div>Account</div>
+    <main>
+      <section>
+        <article>
+          <form onSubmit={(e) => e.preventDefault()} className="form-itself">
+            <h1 className="text-clr-5 text-center font-semibold tracking-wider text-2xl md:text-4xl">
+              Edit Username
+            </h1>
+            <article className="mt-5 form-center">
+              <div className="form-group">
+                  <label htmlFor="user">Username</label>
+                  <input type="text" id="user"
+                      autoComplete="off"
+                      placeholder="kawojue_"
+                      className={`border-2 ${validUser ?
+                      'border-green-400' : 'border-red-400'}`}
+                      value={user} ref={userRef}
+                      onChange={e => setUser(e.target.value)}
+                      aria-invalid={validUser ? "false" : "true"}
+                      aria-describedby="uidnote" max={89} />
+              </div>
+              <button
+                    className="btn" disabled={!validUser}
+                    onClick={async () => await handleUsername()}>
+                        {btnLoading ? 'Changing...' : 'Save'}
+                    </button>
+            </article>
+          </form>
+        </article>
+        <article>
+          <h2>Change Username</h2>
+        </article>
+        <article>
+          <h2>Disable Account</h2>
+        </article>
+      </section>
+    </main>
   )
 }
 
