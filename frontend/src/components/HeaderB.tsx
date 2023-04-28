fed3d46eb7245e6bf2efff7e0bad9dda9cf1ede4
Raheem Kawojue <kawojue08@gmail.com>
2023-04-28 13:50:25 +0100

    edit

--- frontend/src/components/HeaderB.tsx
+++ frontend/src/components/HeaderB.tsx
@@ -1,13 +1,15 @@
 import Home from './Home'
+import useAuth from '@/hooks/useAuth'
 
-const HeaderB:React.FC<{ logout: () => Promise<void>}> = ({ logout }) => {
+const HeaderB:React.FC= () => {
+    const { handleLogout }: any = useAuth()
     return (
         <header className="w-full">
             <nav className="nav md:px-10">
                 <Home/>
                 <ul>
                     <li>
-                        <button type="button" onClick={async () => await logout()}
+                        <button type="button" onClick={async () => await handleLogout()}
                         className="nav-btn  bg-red-500 hover:bg-red-600 hover:text-clr-3">
                             Logout
                         </button>
