c3fad43ac383b1565507293bf77477b345001c94
Raheem Kawojue <kawojue08@gmail.com>
2023-04-30 11:03:53 +0100

    refractored

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,7 @@ import '@/styles/globals.css'
 import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
-import 'react-toastify/dist/ReactToastify.css'
 import { AuthProvider } from '@/hooks/useAuth'
-import { ToastContainer } from 'react-toastify'
-import { Analytics } from '@vercel/analytics/react'
 import { initGA, logPageView } from '@/utils/analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -18,10 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Layout>
-        <Component {...pageProps}>
-          <Analytics />
-          <ToastContainer />
-        </Component>
+        <Component {...pageProps} />
       </Layout>
     </AuthProvider>
   )
