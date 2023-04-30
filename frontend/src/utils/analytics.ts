ed9053f83f993617d7fb1f7afc7143b1dd90160a
Raheem Kawojue <kawojue08@gmail.com>
2023-04-30 10:17:16 +0100

    [add] google analytics

--- frontend/src/utils/analytics.ts
+++ frontend/src/utils/analytics.ts
@@ -2,9 +2,9 @@ import ReactGA from "react-ga";
 
 export const initGA = () => {
     ReactGA.initialize("G-G5V9QPP7DJ")
-};
+}
 
 export const logPageView = () => {
-    ReactGA.set({ page: window.location.pathname })
     ReactGA.pageview(window.location.pathname)
-};
+    ReactGA.set({ page: window.location.pathname })
+}
