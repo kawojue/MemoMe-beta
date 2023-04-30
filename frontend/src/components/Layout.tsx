3be0f22ca991b949945e81aa583409e44e950d18
Raheem Kawojue <kawojue08@gmail.com>
2023-04-30 11:03:38 +0100

    [add] components

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,15 @@
 import Meta from "./Meta"
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+import { Analytics } from '@vercel/analytics/react'
+
 
 const Layout = ({ children }: { children: React.ReactElement }) => {
     return (
         <>
-            <Meta /> 
+            <Meta />
+            <Analytics />
+            <ToastContainer />
             { children }
         </>
     )
