08fc93f8b0a497c2a55018252399934fc5afbe8a
Raheem Kawojue <kawojue08@gmail.com>
2023-05-05 23:50:01 +0100

    [add] icon

--- frontend/src/utils/icons.ts
+++ frontend/src/utils/icons.ts
@@ -1,6 +1,6 @@
 import { GiCancel } from 'react-icons/gi'
-import { BsFacebook, BsFillRocketTakeoffFill } from 'react-icons/bs'
 import { FaShare, FaMehRollingEyes, FaUserAlt } from 'react-icons/fa'
+import { BsFacebook, BsFillRocketTakeoffFill, BsSearch } from 'react-icons/bs'
 import { AiFillEye, AiOutlineDownload, AiFillCopy,
     AiOutlineWhatsApp,AiFillEyeInvisible, AiOutlineTwitter } from 'react-icons/ai'
 
@@ -8,4 +8,4 @@ import { AiFillEye, AiOutlineDownload, AiFillCopy,
 export { AiFillEye, AiOutlineDownload, AiFillCopy,
     AiOutlineWhatsApp, BsFacebook, GiCancel, FaShare,
     FaMehRollingEyes, BsFillRocketTakeoffFill,
-    AiFillEyeInvisible, AiOutlineTwitter, FaUserAlt }
+    AiFillEyeInvisible, AiOutlineTwitter, FaUserAlt, BsSearch }
