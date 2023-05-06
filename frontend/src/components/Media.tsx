f7ea9de4b5cce2e25e725eab0aa3e657da274d82
Raheem Kawojue <kawojue08@gmail.com>
2023-05-06 13:13:34 +0100

    .

--- frontend/src/components/Media.tsx
+++ frontend/src/components/Media.tsx
@@ -35,24 +35,24 @@ const Media: React.FC<{ memo: any }> = ({ memo }) => {
             setThumbnailSrc(canvasRef.current.toDataURL())
         }
     }, [canvasRef])
-    
+
     return (
         <div className="media">
             {memo?.media && <>
-                {getMediaType(memo?.media.secure_url) === "video" ?
+                {getMediaType(memo?.media.secure_url) === "image" ?
                     <img onDoubleClick={() => setBlur(!blur)}
                         className={`image ${blur && 'blur-md'}`}
                         src={memo?.media.secure_url}
                         alt={memo?.idx} loading="lazy" /> :
                     <>
                         <video ref={videoRef}
-                        src={memo?.media.secure_url}
-                        onLoadedMetadata={loadMetadata} />
+                            src={memo?.media.secure_url}
+                            onLoadedMetadata={loadMetadata} />
                         <canvas ref={canvasRef} />
                         <img src={thumbnailSrc}
-                        alt={memo?.idx} loading='lazy'
-                        onDoubleClick={() => setBlur(!blur)}
-                        className={`image ${blur && 'blur-md'}`} />
+                            alt={memo?.idx} loading='lazy'
+                            onDoubleClick={() => setBlur(!blur)}
+                            className={`image ${blur && 'blur-md'}`} />
                     </>}
             </>}
         </div>
