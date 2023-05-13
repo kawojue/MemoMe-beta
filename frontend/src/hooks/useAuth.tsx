d4a662b08046e1fdb38f9b06d22a33cd7d0a21ce
Raheem Kawojue <kawojue08@gmail.com>
2023-05-13 20:35:28 +0100

    refractored

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -24,6 +24,7 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
     const [userId, setUserId] = useState<string>('')
     const [toggles, setToggles] = useState<any>(null)
     const [showPswd, setShowPswd] = useState<boolean>(false)
+    const [btnLoading, setBtnLoading] = useState<boolean>(false)
 
     const [email, setEmail] = useState<string>('')
     const [validEmail, setValidEmail] = useState<boolean>(false)
@@ -39,8 +40,6 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
     const [confirmPswd, setConfirmPswd] = useState<string>('')
     const [validPswd, setValidPswd] = useState<boolean>(false)
 
-    const [btnLoading, setBtnLoading] = useState<boolean>(false)
-
     useEffect(() => {
         const storedToken = JSON.parse(localStorage.getItem('token') as string)
         const storedToggles = JSON.parse(localStorage.getItem('toggles') as any)
@@ -53,16 +52,6 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
             }
         }
     }, [router])
-
-    const throwError = (err: any) => {
-        const msg = err.response?.data?.msg
-        const action = err.response?.data?.action
-        if (action) {
-            notify(action, msg)
-        } else {
-            notify("error", err.code)
-        }
-    }
 
     useEffect(() => {
         setLoading(true)
@@ -107,6 +96,16 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
         setValidUser(resUser)
     }, [user, email, pswd, confirmPswd])
 
+    const throwError = (err: any) => {
+        const msg = err.response?.data?.msg
+        const action = err.response?.data?.action
+        if (action) {
+            notify(action, msg)
+        } else {
+            notify("error", err.code)
+        }
+    }
+
     const notify = (action: string, msg: string): void => {
         if (action === "success") {
             toast.success(msg, {
@@ -125,164 +124,6 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
         }
     }
 
-    const handleSignup = async (): Promise<void> => {
-        setBtnLoading(true)
-        await axios.post('/account/signup',
-        JSON.stringify({ email, pswd, pswd2: confirmPswd }))
-        .then((res: any) => {
-            setEmail("")
-            setPswd("")
-            setConfirmPswd("")
-            setBtnLoading(false)
-            setTimeout(() => {
-                router.push('/login')
-            }, 2000)
-            const { action, msg }: any = res?.data
-            notify(action, msg)
-        })
-        .catch((err: any) => {
-            throwError(err)
-            setBtnLoading(false)
-        })
-    }
-
-    const handleLogin = async (): Promise<void> => {
-        setBtnLoading(true)
-        await axios.post('/account/login',
-        JSON.stringify({ userId, pswd }))
-        .then((res: any) => {
-            const { action, msg, toggles, token }: any = res?.data
-            setPswd("")
-            setUserId("")
-            setConfirmPswd("")
-            notify(action, msg)
-            setBtnLoading(false)
-            localStorage.setItem('token', JSON.stringify(token))
-            localStorage.setItem('toggles', JSON.stringify(toggles))
-            setTimeout(() => {
-                router.push('/profile')
-            }, 1500)
-        })
-        .catch((err: any) => {
-            throwError(err)
-            setBtnLoading(false)
-        })
-    }
-
-    const handleOtpReq = async (): Promise<void> => {
-        await axios.post(
-            '/account/req-otp',
-            JSON.stringify({ email })
-        )
-        .then((res: any) => {
-            notify("success", "OTP has been sent to your email.")
-        })
-        .catch((err: any) => {
-            throwError(err)
-        })
-    }
-
-    const handlePswdVerify = async (): Promise<void> => {
-        setBtnLoading(true)
-        await axios.post(
-            '/account/password/verify',
-            JSON.stringify({ email, otp })
-        )
-        .then((res: any) => {
-            setEligible(res?.data.verified)
-            setBtnLoading(false)
-            router.push('/password/edit')
-        })
-        .catch((err: any) => {
-            throwError(err)
-            setBtnLoading(false)
-        })
-    }
-
-    const handleLogout = async (): Promise<void> => {
-        await axios.get('/account/logout', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        }).then((res: any) => {
-            localStorage.clear()
-            router.push("/login")
-        }).catch((err: any) => {
-            notify("error", err.code)
-        })
-    }
-
-    const handlePswdReset = async (): Promise<void> => {
-        setBtnLoading(true)
-        await axios.post('/account/password/reset',
-        JSON.stringify({
-            verified: eligle, email,
-            newPswd: pswd, newPswd2: confirmPswd
-        }))
-        .then((res: any) => {
-            setPswd("")
-            setEmail("")
-            setConfirmPswd("")
-            setBtnLoading(false)
-            notify(res.data?.action, res.data?.msg)
-            setTimeout(() => {
-                router.push('/login')
-            }, 1500);
-        })
-        .catch((err: any) => {
-            throwError(err)
-            setBtnLoading(false)
-        })
-    }
-
-    const handleUsername = async (): Promise<void> => {
-        setBtnLoading(true)
-        await axios.post(
-            '/account/edit',
-            JSON.stringify({ newUser: user }),
-            {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            }
-        ).then((res: any) => {
-            setUser("")
-            setBtnLoading(false)
-            notify(res?.data?.action, res?.data?.msg)
-            setTimeout(() => {
-                (async () => await handleLogout())()
-            }, 1000)
-        }).catch((err: any) => {
-            throwError(err)
-            setBtnLoading(false)
-        })
-    }
-
-    const editPassword = async (): Promise<void> => {
-        setBtnLoading(true)
-        await axios.post(
-            '/account/password/change',
-            JSON.stringify({ currentPswd, pswd, pswd2: confirmPswd }),
-            {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            }
-        ).then((res: any) => {
-            setPswd("")
-            setConfirmPswd("")
-            setCurrentPswd("")
-            setBtnLoading(false)
-            notify(res?.data?.action, res?.data?.msg)
-            setTimeout(() => {
-                (async () => await handleLogout())()
-            }, 1000)
-        }).catch((err: any) => {
-            setBtnLoading(false)
-            throwError(err)
-        })
-    }
-
     const updateToggle = (key: string, value: boolean): void => {
         let newToggles: any = JSON.parse(localStorage.getItem('toggles') as any)
         if (key === "pbMedia") {
@@ -297,7 +138,7 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
         localStorage.setItem('toggles', JSON.stringify(newToggles))
     }
 
-    const onCopy = async (value: any) => {
+    const onCopy = async (value: any): Promise<void> => {
         try {
             await navigator.clipboard.writeText(value)
             setCopy("Copied!")
@@ -319,9 +160,150 @@ export const AuthProvider: React.FC<{ children: React.ReactElement }> = ({ child
         return period
     }
 
-    const downloadImage = (url: string) => {
+    const downloadImage = (url: string): void => {
         const splitName = url.split('/')
         saveAs(url, splitName[splitName.length - 1])
+    }
+
+    const handleSignup = async (): Promise<void> => {
+        setBtnLoading(true)
+        await axios.post('/account/signup',
+        JSON.stringify({ email, pswd, pswd2: confirmPswd }))
+        .then((res: any) => {
+            setEmail("")
+            setPswd("")
+            setConfirmPswd("")
+            setTimeout(() => {
+                router.push('/login')
+            }, 2000)
+            const { action, msg }: any = res?.data
+            notify(action, msg)
+        }).catch((err: any) => {
+            throwError(err)
+        }).finally(() => setBtnLoading(false))
+    }
+
+    const handleLogin = async (): Promise<void> => {
+        setBtnLoading(true)
+        await axios.post('/account/login',
+        JSON.stringify({ userId, pswd }))
+        .then((res: any) => {
+            const { action, msg, toggles, token }: any = res?.data
+            setPswd("")
+            setUserId("")
+            setConfirmPswd("")
+            notify(action, msg)
+            localStorage.setItem('token', JSON.stringify(token))
+            localStorage.setItem('toggles', JSON.stringify(toggles))
+            setTimeout(() => {
+                router.push('/profile')
+            }, 1500)
+        }).catch((err: any) => {
+            throwError(err)
+        }).finally(() => setBtnLoading(false))
+    }
+
+    const handleOtpReq = async (): Promise<void> => {
+        await axios.post(
+            '/account/req-otp',
+            JSON.stringify({ email })
+        ).then((res: any) => {
+            notify("success", "OTP has been sent to your email.")
+        }).catch((err: any) => {
+            throwError(err)
+        })
+    }
+
+    const handlePswdVerify = async (): Promise<void> => {
+        setBtnLoading(true)
+        await axios.post(
+            '/account/password/verify',
+            JSON.stringify({ email, otp })
+        )
+        .then((res: any) => {
+            setEligible(res?.data.verified)
+            router.push('/password/edit')
+        })
+        .catch((err: any) => {
+            throwError(err)
+        }).finally(() => setBtnLoading(false))
+    }
+
+    const handleLogout = async (): Promise<void> => {
+        await axios.get('/account/logout', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((res: any) => {
+            localStorage.clear()
+            router.push("/login")
+        }).catch((err: any) => {
+            throwError(err)
+        })
+    }
+
+    const handlePswdReset = async (): Promise<void> => {
+        setBtnLoading(true)
+        await axios.post('/account/password/reset',
+        JSON.stringify({
+            verified: eligle, email,
+            newPswd: pswd, newPswd2: confirmPswd
+        }))
+        .then((res: any) => {
+            setPswd("")
+            setEmail("")
+            setConfirmPswd("")
+            notify(res.data?.action, res.data?.msg)
+            setTimeout(() => {
+                router.push('/login')
+            }, 1500);
+        }).catch((err: any) => {
+            throwError(err)
+        }).finally(() => setBtnLoading(false))
+    }
+
+    const handleUsername = async (): Promise<void> => {
+        setBtnLoading(true)
+        await axios.post(
+            '/account/edit',
+            JSON.stringify({ newUser: user }),
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        ).then((res: any) => {
+            setUser("")
+            notify(res?.data?.action, res?.data?.msg)
+            setTimeout(() => {
+                (async () => await handleLogout())()
+            }, 1000)
+        }).catch((err: any) => {
+            throwError(err)
+        }).finally(() => setBtnLoading(false))
+    }
+
+    const editPassword = async (): Promise<void> => {
+        setBtnLoading(true)
+        await axios.post(
+            '/account/password/change',
+            JSON.stringify({ currentPswd, pswd, pswd2: confirmPswd }),
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        ).then((res: any) => {
+            setPswd("")
+            setConfirmPswd("")
+            setCurrentPswd("")
+            notify(res?.data?.action, res?.data?.msg)
+            setTimeout(() => {
+                (async () => await handleLogout())()
+            }, 1000)
+        }).catch((err: any) => {
+            throwError(err)
+        }).finally(() => setBtnLoading(false))
     }
 
     return (
