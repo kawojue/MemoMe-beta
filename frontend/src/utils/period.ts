03921799dd54c625bd03909602266d110501a764
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 23:49:36 +0100

    [add] period

---
+++ frontend/src/utils/period.ts
@@ -0,0 +1,11 @@
+import { formatDistanceToNow, parseISO } from "date-fns"
+
+const getPeriod = (timestamp: string): string => {
+    let period: string = ''
+    if (timestamp) {
+        const date: Date = parseISO(timestamp)
+        const timePeriod: string = formatDistanceToNow(date)
+        period = `${timePeriod} ago..`
+    }
+    return period
+}
