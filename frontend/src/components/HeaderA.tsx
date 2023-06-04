24e77ce49e909a7f2c2d50e6e2d543620a551ddf
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 23:03:38 +0100

    [edit] nav

--- frontend/src/components/HeaderA.tsx
+++ frontend/src/components/HeaderA.tsx
@@ -8,7 +8,7 @@ const HeaderA = ({ get = "login" }: { get?: string }) => {
             <nav className="nav md:px-10">
                 <Home />
                 <Link href={`/${get.replace(" ", "")}`}
-                className="nav-btn hover:text-clr-7 hover:bg-clr-8">
+                className="nav-btn bg-clr-2 hover:text-clr-7 hover:bg-clr-8">
                     {`${get.charAt(0).toUpperCase()}${get.slice(1)}`}
                 </Link>
             </nav>
