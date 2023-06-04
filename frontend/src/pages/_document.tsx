403a3649af6c71d3b7b211b837f0fa18c28322ea
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 22:13:03 +0100

    [edit]

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Meta from '@/components/Meta'
+import { questrial } from '../../public/fonts'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -6,7 +7,7 @@ export default function Document() {
     <Html lang="en">
       <Head/>
       <Meta />
-      <body>
+      <body className={questrial.className}>
         <Main />
         <NextScript />
       </body>
