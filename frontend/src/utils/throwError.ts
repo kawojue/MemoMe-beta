1b0b3802c1a08d3cb7b6d0ce9d4edec15d0c33c4
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 23:45:55 +0100

    [add] throwErr

---
+++ frontend/src/utils/throwError.ts
@@ -0,0 +1,13 @@
+import notify from "./notify"
+
+const throwError = (err: any) => {
+    const msg = err.response?.data?.msg
+    const action = err.response?.data?.action
+    if (action) {
+        notify(action, msg)
+    } else {
+        notify("error", err.code)
+    }
+}
+
+export default throwError
