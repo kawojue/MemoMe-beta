32a3c0a40b74317f612e3663331a848d6cab1c3f
Raheem Kawojue <kawojue08@gmail.com>
2023-06-04 23:45:26 +0100

    [add] download

---
+++ frontend/src/utils/download.ts
@@ -0,0 +1,6 @@
+import { saveAs } from 'file-saver'
+
+const downloadImage = (url: string): void => {
+    const splitName: string[] = url.split('/')
+    saveAs(url, splitName[splitName.length - 1])
+}
