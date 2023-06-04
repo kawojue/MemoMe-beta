7aa1e91e6916cb4978d02f1de0e5c7193079476b
Raheem Kawojue <kawojue08@gmail.com>
2023-06-05 00:36:10 +0100

    [edit] styles

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -5,7 +5,8 @@ import Meta from "@/components/Meta"
 import useAuth from "@/hooks/useAuth"
 import Header from "@/components/HeaderA"
 import PswdButton from '@/components/PswdBtn'
-import { ToastContainer } from 'react-toastify'
+import { SpinnerTwo } from '@/components/Spinner'
+import { inter } from '../../public/fonts'
 
 const login = () => {
     const {
@@ -25,7 +26,6 @@ const login = () => {
         <>
             <Meta title="Login" />
             <Header get='sign up'/>
-            <ToastContainer />
             <form onSubmit={(e) => e.preventDefault()}
             className="form-itself">
                 <h1 className="form-h1 md:text-4xl">
@@ -37,27 +37,29 @@ const login = () => {
                         <input type="text" id="userId"
                             autoComplete="off" max={23}
                             value={userId} ref={userRef}
-                            onChange={e => setUserId(e.target.value)} />
+                            onChange={e => setUserId(e.target.value)}
+                            className={inter.className}/>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
                         <div className="pswd-container">
                             <input max={32} value={pswd}
                             onChange={(e) => setPswd(e.target.value)}
-                            type={`${showPswd ? 'text': 'password'}`} />
+                            type={`${showPswd ? 'text': 'password'}`}
+                            className={inter.className}/>
                             <PswdButton get={showPswd} set={setShowPswd} />
                         </div>
                     </div>
                     <article className="flex justify-end">
                         <Link href="/password/reset"
-                        className="text-clr-3 hover:text-clr-5 trans">
+                        className="text-clr-1 hover:text-clr-6 underline trans">
                             Forgot Password?
                         </Link>
                     </article>
                     <button
                     className="btn" disabled={!isValid}
                     onClick={async () => await handleLogin()}>
-                        {btnLoading ? 'Authenticating...' : 'Login'}
+                        {btnLoading ? <SpinnerTwo /> : 'Login'}
                     </button>
                 </article>
             </form>
