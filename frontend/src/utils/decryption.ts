6607de095d5a92611d1b448b0214884fd5268858
Raheem Kawojue <kawojue08@gmail.com>
2023-06-05 20:42:27 +0100

    [edit] refractored

--- frontend/src/utils/decryption.ts
+++ frontend/src/utils/decryption.ts
@@ -1,8 +1,5 @@
 const crypt = require('text-encryption')
 
-const decrypt = (content: string): string => {
-    const text: string = crypt.decrypt(content, process.env.NEXT_PUBLIC_STRING_KEY)
-    return text
-}
+const decrypt = (content: string): string => crypt.decrypt(content, process.env.NEXT_PUBLIC_STRING_KEY)
 
 export default decrypt
