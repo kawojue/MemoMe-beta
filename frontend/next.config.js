382078f0ffed4c4c5526a441d8291c0f377b86b0
Raheem Kawojue <kawojue08@gmail.com>
2023-06-05 22:40:55 +0100

    [add] remote image

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/kawojue/image/upload/**',
+        },
+    ],
+  },
 }
 
 module.exports = nextConfig
