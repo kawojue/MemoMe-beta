aecdcf0b9acf180fe12abccf3c99a3096f2a84e5
Raheem Kawojue <kawojue08@gmail.com>
2023-06-05 20:42:04 +0100

    [edit] refractored

--- frontend/src/utils/notify.ts
+++ frontend/src/utils/notify.ts
@@ -1,21 +1,4 @@
 import { toast } from "react-hot-toast"
-// const notify = (action: string, msg: string): void => {
-//     if (action === "success") {
-//         toast.success(msg, {
-//             position: toast.POSITION.TOP_RIGHT
-//         })
-//     }
-//     if (action === "warning") {
-//         toast.warning(msg, {
-//             position: toast.POSITION.TOP_LEFT
-//         })
-//     }
-//     if (action === "error") {
-//         toast.error(msg, {
-//             position: toast.POSITION.TOP_LEFT
-//         })
-//     }
-// }
 
 const notify = (action: string, msg: string) => {
     if (action === "success") {
@@ -30,6 +13,5 @@ const notify = (action: string, msg: string) => {
         })
     }
 }
-
 
 export default notify
