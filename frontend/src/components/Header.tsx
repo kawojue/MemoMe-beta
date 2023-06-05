a926a7a2601514d0156d73e8344b2feb91d62526
Raheem Kawojue <kawojue08@gmail.com>
2023-06-05 21:23:48 +0100

    [add] headers

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,35 @@
+import Home from './Home'
+import Link from 'next/link'
+import useAuth from '@/hooks/useAuth'
+
+const HeaderA: React.FC<{ get?: string }> = ({ get = "login" }) => {
+    return (
+        <header className="w-full">
+            <nav className="nav md:px-10">
+                <Home />
+                <Link href={`/${get.replace(" ", "")}`}
+                className="nav-btn bg-clr-2 hover:text-clr-7 hover:bg-clr-8">
+                    {`${get.charAt(0).toUpperCase()}${get.slice(1)}`}
+                </Link>
+            </nav>
+        </header>
+    )
+}
+
+const HeaderB: React.FC = () => {
+    const { handleLogout }: any = useAuth()
+
+    return (
+        <header className="w-full">
+            <nav className="nav md:px-10">
+                <Home/>
+                <button type="button" onClick={async () => await handleLogout()}
+                className="nav-btn hover:text-clr-7 hover:bg-clr-8">
+                    Logout
+                </button>
+            </nav>
+        </header>
+    )
+}
+
+export { HeaderA, HeaderB }
