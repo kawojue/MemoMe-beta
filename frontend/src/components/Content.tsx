7e726b2b7b686879640c5ad39437e9c21e2e9c5c
Raheem Kawojue <kawojue08@gmail.com>
2023-06-06 12:32:36 +0100

    [edit] structure

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -3,35 +3,40 @@ import Media from './Media'
 import getPeriod from '@/utils/period'
 import download from '@/utils/download'
 import decrypt from "@/utils/decryption"
+import { questrial } from '../../public/fonts'
 import { AiOutlineDownload } from '../../public/icons'
 
 const Content: React.FC<{ memos: any[] }> = ({ memos }) => {
     return (
         <>
         {memos?.map((memo: any) => (
-            <article key={memo.idx} className="profile-msg">
-                <p className="period">
-                    {getPeriod(memo?.time)}
-                </p>
-                {memo?.content &&
-                    <div className="texts">
-                        {decrypt(memo?.content || '')?.split('\n')?.map(
-                            (content: string, index: number) => (
-                                <span className="texts" key={index}>
-                                    {String(content)}
-                                </span>
-                            ))}
-                    </div>}
-                {memo?.media && <Media memo={memo}/>}
+            <>
+                <section className="flex flex-col gap-0.5">
+                <article key={memo.idx} className="profile-msg card">
+                    <p className="period">
+                        {getPeriod(memo?.time)}
+                    </p>
+                    {memo?.content &&
+                        <div className="texts">
+                            {decrypt(memo?.content || '')?.split('\n')?.map(
+                                (content: string, index: number) => (
+                                    <span className="texts" key={index}>
+                                        {String(content)}
+                                    </span>
+                                ))}
+                        </div>}
+                    {memo?.media && <Media memo={memo}/>}
+                </article>
                 {memo?.media &&
-                    <div className="mt-2 flex justify-center">
-                        <button onClick={() => download(memo?.media.secure_url)}
-                            className="bg-clr-3 rounded-lg px-2 py-1 w-full font-semibold font-poppins flex items-center gap-2 justify-center text-clr-0 tracking-wider">
-                            <AiOutlineDownload />
-                            <span>Download Content</span>
-                        </button>
-                    </div>}
-            </article>
+                        <div className="mt-2 flex justify-center">
+                            <button onClick={() => download(memo?.media.secure_url)}
+                                className={`${questrial.className} bg-clr-3 rounded-lg px-2 py-1 w-full font-semibold flex items-center gap-2 justify-center text-clr-0 tracking-wider`}>
+                                <AiOutlineDownload />
+                                <span>Download Content</span>
+                            </button>
+                        </div>}
+                </section>
+            </>
         ))}
         </>
     )
