be31c7aec7b668804cfe316fd2417ccab5bce4a0
Raheem Kawojue <kawojue08@gmail.com>
2023-06-06 13:34:44 +0100

    [edit] refractored

--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import PswdButton from './PswdBtn'
 import useAuth from "@/hooks/useAuth"
+import { SpinnerTwo } from './Spinner'
 import axios from '@/pages/api/instance'
 import { useEffect, useState } from 'react'
+import { inter } from '../../public/fonts'
 
 const Account: React.FC = () => {
   const {
@@ -37,32 +39,31 @@ const Account: React.FC = () => {
     <main className="mb-10">
       <section>
         <article>
-          <form onSubmit={(e) => e.preventDefault()} className="form-itself">
+          <form onSubmit={(e) => e.preventDefault()} className="card form-itself">
             <h1 className="form-h1 md:text-4xl">
               Edit Username
             </h1>
             <article className="mt-5 form-center">
               <div className="form-group">
-                  <label htmlFor="user">Username</label>
                   <input type="text" id="user"
-                      autoComplete="off"
-                      placeholder="kawojue"
-                      className={`border-2 ${validUser ?
-                      'border-green-400' : 'border-red-400'}`}
-                      value={user} ref={userRef}
-                      onChange={e => setUser(e.target.value)}
-                      aria-invalid={validUser ? "false" : "true"}
-                      aria-describedby="uidnote" max={89} />
+                  autoComplete="off"
+                  placeholder="kawojue"
+                  className={`border-2 ${validUser ?
+                  'border-clr-4' : 'border-clr-8'}`}
+                  value={user} ref={userRef}
+                  onChange={e => setUser(e.target.value)}
+                  aria-invalid={validUser ? "false" : "true"}
+                  aria-describedby="uidnote" max={89} />
               </div>
               <button
                     className="btn" disabled={!validUser}
                     onClick={async () => await handleUsername()}>
-                        {btnLoading ? 'Editing...' : 'Save'}
+                        {btnLoading ? <SpinnerTwo /> : 'Save'}
                     </button>
             </article>
           </form>
         </article>
-        <form onSubmit={(e) => e.preventDefault()} className="form-itself">
+        <form onSubmit={(e) => e.preventDefault()} className="card form-itself">
             <h1 className="form-h1 md:text-4xl">
               Change Password
             </h1>
@@ -82,8 +83,8 @@ const Account: React.FC = () => {
                     <input max={32} value={pswd}
                     onChange={(e) => setPswd(e.target.value)}
                     type={`${showPswd ? 'text': 'password'}`}
-                    className={`border-2 ${validPswd ?
-                    'border-green-400' : 'border-red-400'}`}
+                    className={`${inter.className} ${validPswd ?
+                    'border-clr-4' : 'border-clr-8'}`}
                     aria-invalid={validPswd ? "false" : "true"}
                     aria-describedby="uidnote" />
                     <PswdButton get={showPswd} set={setShowPswd} />
@@ -95,20 +96,20 @@ const Account: React.FC = () => {
                     <input max={32} value={confirmPswd}
                     type='password' aria-describedby="uidnote"
                     onChange={(e) => setConfirmPswd(e.target.value)}
-                    className={`border-2 ${validPswd ?
-                    'border-green-400' : 'border-red-400'}`}
+                    className={`${inter.className} ${validPswd ?
+                    'border-clr-4' : 'border-clr-8'}`}
                     aria-invalid={validPswd ? "false" : "true"} />
                 </div>
-                </div>
+              </div>
               <button className="btn" disabled={!isValidToEditPswd}
               onClick={async () => await editPassword()}>
-                {btnLoading ? 'Encrypting...' : 'Change'}
+                {btnLoading ? <SpinnerTwo /> : 'Change'}
               </button>
             </article>
           </form>
-        <article className="mt-10 form-itself">
+        <article className="mt-10 form-itself card">
           <div className="toggle-container">
-            <h2 className="toggle-h2 text-red-400">
+            <h2 className="toggle-h2 text-red-600">
               Disable Account (Temporary)
             </h2>
             <label className="switch">
