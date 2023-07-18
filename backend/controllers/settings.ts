a873fa04900e0f104a23063f0e01d01d5d88c3ec
Raheem Kawojue <kawojue08@gmail.com>
2023-07-18 11:35:21 +0100

    refactored

--- backend/controllers/settings.ts
+++ backend/controllers/settings.ts
@@ -2,52 +2,25 @@ import bcrypt from 'bcrypt'
 import { IRequest } from '../type'
 import { Response } from 'express'
 import User from '../models/UserModel'
+import {
+    ERROR, INC_PSWD, SMTH_WRONG, SUCCESS
+} from '../utils/modal'
 const asyncHandler = require('express-async-handler')
 
 const editPswd = asyncHandler(async (req: IRequest, res: Response) => {
     const { currentPswd, pswd, pswd2 }: any = req.body
 
-    if (!currentPswd) {
-        return res.status(400).json({
-            success: false,
-            action: 'error',
-            msg: 'Old password is required.'
-        })
-    }
+    if (!currentPswd) return res.status(400).json({ ...ERROR, msg: 'Old password is required.' })
 
-    if (pswd !== pswd2) {
-        return res.status(400).json({
-            success: false,
-            action: 'error',
-            msg: 'Password does not match.'
-        })
-    }
+    if (pswd !== pswd2) return res.status(400).json({ ...ERROR, msg: 'Password does not match.' })
 
-    if (currentPswd === pswd === pswd2) {
-        return res.status(400).json({
-            success: false,
-            action: 'error',
-            msg: 'Your Old and New Passowrds are matched.'
-        })
-    }
+    if (currentPswd === pswd === pswd2) return res.status(400).json({ ...ERROR, msg: 'Your old and new passowrds are matched.' })
 
-    const account: any = await User.findOne({ user: req?.user?.user }).exec()
-    if (!account) {
-        return res.status(404).json({
-            success: false,
-            action: 'error',
-            msg: 'Something went wrong..'
-        })
-    }
+    const account: any = await User.findOne({ user: req.user?.user }).exec()
+    if (!account) return res.status(404).json(SMTH_WRONG)
 
     const isMatch = await bcrypt.compare(currentPswd, account.password)
-    if (!isMatch) {
-        return res.status(401).json({
-            success: false,
-            action: 'error',
-            msg: 'Incorrect password.'
-        })
-    }
+    if (!isMatch) return res.status(401).json(INC_PSWD)
 
     const salt: string = await bcrypt.genSalt(10)
     const hashedPswd: string = await bcrypt.hash(pswd, salt)
@@ -55,86 +28,56 @@ const editPswd = asyncHandler(async (req: IRequest, res: Response) => {
     account.password = hashedPswd
     await account.save()
 
-    res.status(200).json({
-        success: true,
-        action: 'success',
-        msg: 'Password changed successfully.'
-    })
+    res.status(200).json({ ...SUCCESS, msg: 'Password updated successfully.' })
 })
 
 const toggleDisability = asyncHandler(async (req: IRequest, res: Response) => {
     const { tgDisability }: any = req.body
 
-    const account: any = await User.findOne({ user: req?.user?.user }).exec()
-    if (!account) {
-        return res.status(404).json({
-            success: false,
-            action: 'error',
-            msg: 'Something went wrong..'
-        })
-    }
+    const account: any = await User.findOne({ user: req.user?.user }).exec()
+    if (!account) return res.status(404).json(SMTH_WRONG)
 
     account.disabled = tgDisability as boolean
     await account.save()
-    res.status(200).json({ success: true })
+
+    res.status(200).json(SUCCESS)
 })
 
 const togglePbMedia = asyncHandler(async (req: IRequest, res: Response) => {
     const { tgPbMedia }: any = req.body
 
-    const account: any = await User.findOne({ user: req?.user?.user }).exec()
-    if (!account) {
-        return res.status(404).json({
-            success: false,
-            action: 'error',
-            msg: 'Something went wrong..'
-        })
-    }
+    const account: any = await User.findOne({ user: req.user?.user }).exec()
+    if (!account) return res.status(404).json(SMTH_WRONG)
 
     account.pbMedia = tgPbMedia as boolean
     await account.save()
+
     res.sendStatus(200)
 })
 
 const togglePbContent = asyncHandler(async (req: IRequest, res: Response) => {
     const { tgPbContent }: any = req.body
 
-    const account: any = await User.findOne({ user: req?.user?.user }).exec()
-    if (!account) {
-        return res.status(404).json({
-            success: false,
-            action: 'error',
-            msg: 'Something went wrong..'
-        })
-    }
+    const account: any = await User.findOne({ user: req.user?.user }).exec()
+    if (!account) return res.status(404).json(SMTH_WRONG)
 
     account.pbContent = tgPbContent as boolean
     await account.save()
+
     res.sendStatus(200)
 })
 
 const toggleMessage = asyncHandler(async (req: IRequest, res: Response) => {
     const { pbMsg }: any = req.body
 
-    if (pbMsg?.length > 150) {
-        return res.status(400).json({
-            success: false,
-            action: 'error',
-            msg: 'Lmao..! Not saved!'
-        })
-    }
+    if (pbMsg?.length > 150) return res.status(400).json({ ...ERROR, msg: 'Characters too long. Not saved!' })
 
-    const account: any = await User.findOne({ user: req?.user?.user }).exec()
-    if (!account) {
-        return res.status(404).json({
-            success: false,
-            action: 'error',
-            msg: 'Something went wrong..'
-        })
-    }
+    const account: any = await User.findOne({ user: req.user?.user }).exec()
+    if (!account) return res.status(404).json(SMTH_WRONG)
 
     account.pbMsg = pbMsg as string
     await account.save()
+
     res.sendStatus(200)
 })
 
