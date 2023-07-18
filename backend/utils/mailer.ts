03dc8d12d04ceea77b7fbdd951d89365062b69e5
Raheem Kawojue <kawojue08@gmail.com>
2023-07-18 09:51:46 +0100

    refactored

--- backend/utils/mailer.ts
+++ backend/utils/mailer.ts
@@ -1,3 +1,4 @@
+import { IMailer } from '../type'
 import transporter from '../config/mailTransport'
 
 export default async function mailer({ senderName, to, subject, text }: IMailer): Promise<void> {
