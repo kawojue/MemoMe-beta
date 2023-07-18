92cffedcee3c917b2dac782354c4d6993230b23e
Raheem Kawojue <kawojue08@gmail.com>
2023-07-18 09:11:42 +0100

    refactored

--- backend/middlewares/jwtVerify.ts
+++ backend/middlewares/jwtVerify.ts
@@ -1,18 +1,22 @@
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import User from '../models/UserModel'
-import { Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
 const asyncHandler = require('express-async-handler')
 
 dotenv.config()
 
-const jwtVerify = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
+interface IRequest extends Request {
+    user: any
+}
+
+const jwtVerify = asyncHandler(async (req: IRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers?.authorization
 
     if (!authHeader?.startsWith('Bearer')) {
         return res.status(401).json({
             success: false,
-            action: "warning",
+            action: "error",
             msg: "Not accessible."
         })
     }
@@ -24,9 +28,9 @@ const jwtVerify = asyncHandler(async (req: any, res: Response, next: NextFunctio
         async (err: any, decoded: any) => {
             if (err) {
                 return res.status(403).json({
-                success: false,
-                action: "error",
-                msg: "Forbidden"
+                    success: false,
+                    action: "error",
+                    msg: "Forbidden"
                 })
             }
             req.user = await User.findOne({ user: decoded.user }).select('-password').exec()
