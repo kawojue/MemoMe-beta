2365b4e1c5681eef5f520f353bdb24f7a77c670f
Raheem Kawojue <kawojue08@gmail.com>
2023-07-18 09:49:18 +0100

    refactored

--- backend/middlewares/jwtVerify.ts
+++ backend/middlewares/jwtVerify.ts
@@ -1,38 +1,24 @@
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
+import { IRequest } from '../type'
 import User from '../models/UserModel'
-import { Request, Response, NextFunction } from 'express'
+import { ACCESS_DENIED } from '../utils/modal'
+import { Response, NextFunction } from 'express'
 const asyncHandler = require('express-async-handler')
 
 dotenv.config()
 
-interface IRequest extends Request {
-    user: any
-}
-
 const jwtVerify = asyncHandler(async (req: IRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers?.authorization
-
-    if (!authHeader?.startsWith('Bearer')) {
-        return res.status(401).json({
-            success: false,
-            action: "error",
-            msg: "Not accessible."
-        })
-    }
+    if (!authHeader?.startsWith('Bearer')) return res.status(401).json(ACCESS_DENIED)
 
     const token: string = authHeader.split(' ')[1]
     jwt.verify(
         token,
-        process.env.JWT_SECRET as string,
+        process.env.JWT_SECRET,
         async (err: any, decoded: any) => {
-            if (err) {
-                return res.status(403).json({
-                    success: false,
-                    action: "error",
-                    msg: "Forbidden"
-                })
-            }
+            if (err) return res.status(403).json(ACCESS_DENIED)
+
             req.user = await User.findOne({ user: decoded.user }).select('-password').exec()
             next()
         }
