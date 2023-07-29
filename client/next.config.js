d4c8c8d342cdbcb51da660488053bf0b53c16d1b
Raheem Kawojue <kawojue08@gmail.com>
2023-07-30 00:43:21 +0100

    setup: image config

---
+++ client/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/kawojue/image/upload/**',
+            },
+        ],
+    },
+}
+
+module.exports = nextConfig
