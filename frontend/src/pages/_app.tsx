7d3f42362e557bcd3ba6d38cc3b9fc0e2a79a5db
Raheem Muyiwa <kawojue08@gmail.com>
2023-08-10 17:00:07 +0100

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,14 +3,8 @@ import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { AuthProvider } from '@/hooks/useAuth'
-import { initGA, logPageView } from '@/utils/analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  useEffect(() => {
-    initGA()
-    logPageView()
-  }, [])
 
   return (
     <AuthProvider>
