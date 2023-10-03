a3b9e5374b438d28e7d344f7c870e600a0fad48c
Raheem Kawojue <kawojue08@gmail.com>
2023-10-03 14:48:35 +0100

    redeploying

--- backend/dist/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cloudinary_1 = require("cloudinary");
-cloudinary_1.v2.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
-});
-exports.default = cloudinary_1.v2;
--- backend/utils/mailer.ts
+++ backend/utils/mailer.ts
@@ -2,13 +2,10 @@ import { IMailer } from '../type'
 import transporter from '../config/mailTransport'
 
 export default async function mailer({ senderName, to, subject, text }: IMailer): Promise<void> {
-    await transporter.sendMail({
-        from: `${senderName} <${process.env.EMAIL}>`,
-        to,
-        subject,
-        text,
-        headers: {
-            'Content-Type': 'application/text',
-        }
-    })
+  await transporter.sendMail({
+    from: `${senderName} <${process.env.EMAIL}>`,
+    to,
+    subject,
+    html: text,
+  })
 }
